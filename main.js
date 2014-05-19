02600ee54f80c0a597ddc1483dbf51b8620243f1
runningtalus <dave@davemay.org>
2014-05-19 09:56:31 -0600

    Introduction to handlebars.js. First commit

---
+++ main.js
@@ -0,0 +1,21 @@
+$(document).on('ready', function() {
+	var forecasts = [
+	{day: "Sunday", temp: 82},
+	{day: "Monday", temp: 90}, 
+	{day: "Tuesday", temp: 78},
+	{day: "Wednesday", temp: 87},
+	{day: "Thursday", temp: 69},
+	{day: "Friday", temp: 75},
+	{day: "Saturday", temp: 89}
+	];
+
+	var weatherTemplate = $('#weather-template');
+	var templateText = weatherTemplate.html();
+
+	console.log(templateText);
+		// <div id="weather">
+		// 	<b>Day:</b> {{day}}
+		// 	<b>Temperature</b> {{temp}}
+		// </div>
+
+});
